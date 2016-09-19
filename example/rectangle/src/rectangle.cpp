8688b21c20b11e5e9beb267de9518f6f9a9f964f
Yordan Madzhunkov <y.madzhunkov@gmail.com>
2016-09-19 15:33:51 +0300

    Step 2: Make it pass

--- example/rectangle/src/rectangle.cpp
+++ example/rectangle/src/rectangle.cpp
@@ -9,5 +9,6 @@ bool Rectangle::overlap(const Rectangle & other) const {
     bool res = x < other.x + other.width;
     res &= other.x < x + width;
     res &= other.y < y + height;
+    res &= y < other.y + other.height;
     return res; 
 }
