981df40462d77da7d5ca7dacefdd112ee5363eda
Yordan Madzhunkov <y.madzhunkov@gmail.com>
2016-09-19 16:12:48 +0300

    Step 3: Refactoring: method extracted

--- example/rectangle/src/rectangle.cpp
+++ example/rectangle/src/rectangle.cpp
@@ -4,10 +4,12 @@
 Rectangle::Rectangle(int x, int y, int width, int height)
     : x(x), y(y), width(width), height(height) {}
 
+bool overlapX(int a0, int b0, int a1, int b1) {
+    return a0 < b1 && b0 < a1;
+}
 
 bool Rectangle::overlap(const Rectangle & other) const {
-    bool res = x < other.x + other.width;
-    res &= other.x < x + width;
+    bool res = overlapX(x, other.x, x + width, other.x + other.width);
     res &= other.y < y + height;
     res &= y < other.y + other.height;
     return res; 
