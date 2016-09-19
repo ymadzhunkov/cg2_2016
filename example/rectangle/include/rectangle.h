d6af1530dc30f9e66424411d3ae7f0b74eb7ce38
Yordan Madzhunkov <y.madzhunkov@gmail.com>
2016-09-19 15:13:24 +0300

    Step 2: Make it pass

--- example/rectangle/include/rectangle.h
+++ example/rectangle/include/rectangle.h
@@ -3,4 +3,5 @@ struct Rectangle {
     Rectangle(int x, int y, int width, int height);
     int x, y;
     int width, height;
+    bool overlap(const Rectangle & other) const;
 };
--- example/rectangle/src/rectangle.cpp
+++ example/rectangle/src/rectangle.cpp
@@ -1,4 +1,10 @@
 #include "rectangle.h"
 
+
 Rectangle::Rectangle(int x, int y, int width, int height)
     : x(x), y(y), width(width), height(height) {}
+
+
+bool Rectangle::overlap(const Rectangle & other) const {
+    return true;
+}
