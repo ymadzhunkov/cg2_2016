b7dbcd3addf7185f1d561509e8de117a2387ebf1
Yordan Madzhunkov <y.madzhunkov@gmail.com>
2016-09-19 15:23:52 +0300

    Step 1: Add a failing test

--- example/rectangle/test/test_rectangle.cpp
+++ example/rectangle/test/test_rectangle.cpp
@@ -22,3 +22,10 @@ TEST_CASE("Test non overlaping rectangles") {
     bool overlap = a.overlap(b);
     REQUIRE(overlap == false);
 }
+
+TEST_CASE("Test non overlaping rectangles") {
+    Rectangle a(1, 2, 3, 4);
+    Rectangle b(6, 7, 10, 12);
+    bool overlap = a.overlap(b);
+    REQUIRE(overlap == false);
+}
