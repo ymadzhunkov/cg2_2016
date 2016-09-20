1befaca2e452304351600e7b9cc553342efb846b
Yordan Madzhunkov <y.madzhunkov@gmail.com>
2016-09-20 08:52:16 +0300

    Rename function

--- example/rectangle/src/rectangle.cpp
+++ example/rectangle/src/rectangle.cpp
@@ -4,11 +4,11 @@
 Rectangle::Rectangle(int x, int y, int width, int height)
     : x(x), y(y), width(width), height(height) {}
 
-bool overlapX(int a0, int b0, int a1, int b1) {
+bool overlap1D(int a0, int b0, int a1, int b1) {
     return a0 < b1 && b0 < a1;
 }
 
 bool Rectangle::overlap(const Rectangle &other) const {
-    return overlapX(x, other.x, x + width, other.x + other.width) &&
-           overlapX(y, other.y, y + height, other.y + other.height);
+    return overlap1D(x, other.x, x + width, other.x + other.width) &&
+           overlap1D(y, other.y, y + height, other.y + other.height);
 }
